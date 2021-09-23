8ad2db0ab19cd34c619f7932514018c833e9baf7
nang-e <yeanvely@naver.com>
2021-09-23 14:51:37 +0900

    @nang-e

---
+++ kny/8week_ny1.py
@@ -0,0 +1,49 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+import numpy as np
+
+def solution(scores):
+    scores = np.array(scores)
+    answer = []
+    
+    for i in range(len(scores)):
+        x = scores[:,i]
+        y = np.delete(x,i)
+        if (min(x) == x[i]) | (max(x) == x[i]):
+            if x[i] not in y:
+                x = y
+        average = x.mean()
+        print(x)
+    
+        if average>=90:
+            answer.append('A')
+        elif 80<=average<90:
+            answer.append('B')
+        elif 70<=average<80:
+            answer.append('C')
+        elif 50<=average<70:
+            answer.append('D')
+        else:
+            answer.append('F')
+    
+    answer = ''.join(answer)
+    return answer
+
+
+# -
+
+solution(scores)
