f1335f407f9577c3061546c81f9d0071ebb3dd3a
nang-e <yeanvely@naver.com>
2021-11-08 10:50:51 +0900

    @nang-e

---
+++ kny/13week/K번째수_ny1.py
@@ -0,0 +1,26 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+import pandas as pd
+import numpy as np
+
+
+def solution(array, commands):
+    answer = []
+    for i in range(len(commands)):
+        arr = array[commands[i][0]-1:commands[i][1]]
+        arr.sort()
+        answer.append(arr[commands[i][2]-1])
+    return answer
