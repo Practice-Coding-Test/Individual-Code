dd93d952c728724449c198a5c7096126c2e3d6f5
sweetperssimmon <ghdwl1103@naver.com>
2021-11-19 14:46:34 +0900

    @sweetpersimmon

--- hjw/15week/15week_hjw2.py
+++ hjw/15week/15week_hjw2.py
@@ -1,17 +1,7 @@
-import math
-def solution(progresses, speeds):
+def solution(array, commands):
     answer = []
-    temp = []
-    lst = []
-    max_prog = 0
-    max_idx = 0
-    for i in range(len(progresses)):
-        answer.append(math.ceil((100-progresses[i]) / speeds[i]))
-    for idx, ans in enumerate(answer):
-        if ans > max_prog:
-            max_prog = ans
-            temp.append(idx)
-    temp.append(len(answer))
-    for t in range(1,len(temp)):
-        lst.append(temp[t]-temp[t-1])
-    return lst
+    for com in commands:
+        arr = list(array[com[0]-1:com[1]])
+        arr = sorted(arr)
+        answer.append(arr[com[2]-1])
+    return answer
